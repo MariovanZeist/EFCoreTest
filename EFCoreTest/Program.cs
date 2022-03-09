ffe69f731b64d1384f0cf5e67a42eb73108c0258
Mario van Zeist <m.vanzeist@bwb-it.nl>
2022-03-09 13:11:38 +0100

    Add project files.

---
+++ EFCoreTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EFCoreTest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var ctx = new TestDbContext();
+            var roles = await ctx.Users.Where(u => u.Id == Guid.NewGuid()).Select(u => u.Roles.Select(r => r.Name)).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ EFCoreTest/TestDbContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTest
+{
+    public class TestDbContext : DbContext
+{
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Data Source=localhost;Initial Catalog=SimpleStore;Integrated Security=True");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.HasMany(d => d.Roles)
+                    .WithMany(p => p.Users)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "UserRole",
+                        l => l.HasOne<Role>().WithMany().HasForeignKey("RoleId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_UserRoles_Roles"),
+                        r => r.HasOne<User>().WithMany().HasForeignKey("UserId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_UserRoles_Users"),
+                        j =>
+                        {
+                            j.HasKey("UserId", "RoleId").HasName("PK_UserRoles_1");
+
+                            j.ToTable("UserRoles");
+                        });
+            });
+        }
+    }
+
+
+    public partial class User
+    {
+        public User()
+        {
+            Roles = new HashSet<Role>();
+        }
+
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+
+    public partial class Role
+    {
+        public Role()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
